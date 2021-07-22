3d0ccd813fa993a1d57997c9cc0bba9f8a8422a2
Stephan <sgriesel@gmail.com>
2021-07-22 08:01:00 +0200

    test data

---
+++ src/components/InfoForm.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react"
+import { Button, Form, Container, Header } from "semantic-ui-react"
+import axios from "axios"
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: "",
+      age: "",
+      salary: "",
+      hobby: "",
+    }
+  }
+
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+    console.log(this.state)
+
+    axios
+      .post(
+        "https://sheet.best/api/sheets/208a1f15-0739-4d0e-a700-16b34cb621a4",
+        this.state
+      )
+      .then(response => {
+        console.log(response)
+      })
+  }
+
+  render() {
+    const { name, age, salary, hobby } = this.state
+    return (
+      <Container fluid className="container">
+        <Header as="h2">React Google Sheets!</Header>
+        <Form className="form" onSubmit={this.submitHandler}>
+          <Form.Field>
+            <label>Name</label>
+            <input
+              placeholder="Enter your name"
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Age</label>
+            <input
+              placeholder="Enter your age"
+              type="number"
+              name="age"
+              value={age}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Salary</label>
+            <input
+              placeholder="Enter your salary"
+              type="number"
+              name="salary"
+              value={salary}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Hobby</label>
+            <input
+              placeholder="Enter your hobby"
+              type="text"
+              name="hobby"
+              value={hobby}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+
+          <Button color="blue" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    )
+  }
+}
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,37 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
-import { Button, Form, Container, Header } from "semantic-ui-react"
+import InfoForm from "../components/InfoForm"
 
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <Container fluid className="container">
-      <Header as="h2">React Google Sheets!</Header>
-      <Form className="form">
-        <Form.Field>
-          <label>Name</label>
-          <input placeholder="Enter your name" />
-        </Form.Field>
-        <Form.Field>
-          <label>Age</label>
-          <input placeholder="Enter your age" />
-        </Form.Field>
-        <Form.Field>
-          <label>Salary</label>
-          <input placeholder="Enter your salary" />
-        </Form.Field>
-        <Form.Field>
-          <label>Hobby</label>
-          <input placeholder="Enter your hobby" />
-        </Form.Field>
-
-        <Button color="blue" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
+    <InfoForm />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
