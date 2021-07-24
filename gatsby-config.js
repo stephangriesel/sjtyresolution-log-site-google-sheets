2260d6caa8907513957778a832b4146014d74c92
Stephan <sgriesel@gmail.com>
2021-07-24 08:51:15 +0200

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `SJ Tyre Solution`,
+    description: `Logging site for SJ Tyre Solution`,
+    author: `@stephangriesel`,
+    siteUrl: `https://sjtyresolution.consulitate.com/log`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,12 +21,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `SJ Tyre Solution`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
